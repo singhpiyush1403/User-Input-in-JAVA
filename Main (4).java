39849c3f9e79d1ded69e1db10556ccbaae3fcd11
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-23 14:12:03 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,53 @@
+
+//  public class Main{
+//     public static void main(String[] args){
+//     int marks[] = {89,63,39};
+//     for(int i=0; i<3; i++){
+//         System.out.println(marks[i]);
+//     }
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner myobj= new Scanner(System.in);
+//         String UserName;
+//         System.out.println("Enter userName:");
+       
+//         UserName = myobj.nextLine();
+
+//         System.out.println("UserName is :" + UserName);
+//     }
+// }
+
+
+
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner myobj = new Scanner(System.in);
+        
+        int a = 10;
+        int b = 30;
+
+        System.out.println("Enter a number:");
+        int input = myobj.nextInt();  
+
+        System.out.println("The input is: " + input);
+        System.out.println("The sum of a and b is: " + (a * b));  
+    }
+}
+
+
+    
+
+
+
+
+    
+
